3e16de9f07ed64e06236bdbbf35b624054e180b2
3burmest <frederik@burmester.eu>
2015-06-23 17:25:46 +0200

    ShipMove script funktioniert ansatzweise, ne anständige Kamera wäre noch nett

---
+++ SpaceRace/Assets/ShipControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipControl : MonoBehaviour {
+	public int speed = 100;
+	public int rotationSpeed = 10;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		GetComponent<Rigidbody>().AddRelativeTorque(-Vector3.forward * Input.GetAxis("Horizontal") * rotationSpeed);
+		GetComponent<Rigidbody>().AddRelativeTorque(-Vector3.left * Input.GetAxis("Vertical") * (rotationSpeed/2));
+
+		GetComponent<Rigidbody>().velocity = (transform.forward * speed);
+	}
+}
