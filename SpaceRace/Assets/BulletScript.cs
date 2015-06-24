50561c3a18c856a4146db1b7a4e0e6a2ded6d788
3burmest <frederik@burmester.eu>
2015-06-24 20:41:58 +0200

    Blablabla paar sachen verbessert, die Bullets sehen noch nicht so mega aus..

--- SpaceRace/Assets/BulletScript.cs
+++ SpaceRace/Assets/BulletScript.cs
@@ -4,17 +4,22 @@ using System.Collections;
 public class BulletScript : MonoBehaviour {
 
 	public float speed = 10f;
+	public float lifeTime = 2.0F;
 
 	private Rigidbody rb;
+	private float startTime;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
 		rb.AddRelativeForce (new Vector3 (0, 0, 1) * speed, ForceMode.Impulse);
+		startTime = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (lifeTime < Time.time - startTime) {
+			Destroy(gameObject);
+		}
 	}
 }
--- SpaceRace/Assets/SmoothFollow.cs
+++ SpaceRace/Assets/SmoothFollow.cs
@@ -3,49 +3,27 @@ using System.Collections;
 
 public class SmoothFollow : MonoBehaviour {
 	public Transform target;
+	public float followRotationSpeed = 0.1F;
+	public float smoothPositionTime = 0.3F;
+	public int distance = 20;
 
+	Vector3 velocity = Vector3.zero;
+	
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
-	/* This camera smoothes out rotation around the y-axis and height. Horizontal Distance to the target is always fixed.
 
-There are many different ways to smooth the rotation but doing it this way gives you a lot of control over how the camera behaves.
+	void FixedUpdate () {
+		if(Input.GetKey(KeyCode.LeftShift)) {
+			//Nach hinten gucken
+		} else {
+			//Finde ich sieht besser aus
+			transform.rotation = Quaternion.Slerp(transform.rotation, target.rotation, followRotationSpeed);
+			//Wäre eine alternative.. Ist aber miener Meinung nach weniger intuitiv
+			/*transform.LookAt(target.position, target.up);*/
+		}
 
-For every of those smoothed values we calculate the wanted value and the current value. Then we smooth it using the Lerp function. Then we apply the smoothed values to the transform's position. */
-
-// The target we are following var target : Transform; // The distance in the x-z plane to the target var distance = 10.0; // the height we want the camera to be above the target var height = 5.0; // How much we var heightDamping = 2.0; var rotationDamping = 3.0;
-
-// Place the script in the Camera-Control group in the component menu @script AddComponentMenu("Camera-Control/Smooth Follow")
-
-	void LateUpdate () { // Early out if we don't have a target if (!target) return;
-/*
- // Calculate the current rotation angles
- wantedRotationAngle = target.eulerAngles.y;
- wantedHeight = target.position.y + height;
- 
- currentRotationAngle = transform.eulerAngles.y;
- currentHeight = transform.position.y;
- 
- // Damp the rotation around the y-axis
- currentRotationAngle = Mathf.LerpAngle (currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
- 
- // Damp the height
- currentHeight = Mathf.Lerp (currentHeight, wantedHeight, heightDamping * Time.deltaTime);
- 
- // Convert the angle into a rotation
- currentRotation = Quaternion.Euler (0, currentRotationAngle, 0);
- 
- // Set the position of the camera on the x-z plane to:
- // distance meters behind the target
- transform.position = target.position;
- transform.position -= currentRotation * Vector3.forward * distance;
- 
- // Set the height of the camera
- transform.position.y = currentHeight;
- 
- // Always look at the target
- transform.LookAt (target);*/
+		transform.position = Vector3.SmoothDamp(transform.position, target.position - target.forward * distance, ref velocity, smoothPositionTime);
 	}
 }
