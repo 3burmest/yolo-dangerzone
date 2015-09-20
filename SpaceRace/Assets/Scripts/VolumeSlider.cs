e8cff597a8aa2f85a72315371beef731be9f0374
Acrosicious <basti778@gmx.net>
2015-09-21 00:44:03 +0200

    Etwas mehr Ordnung
    -

---
+++ SpaceRace/Assets/Scripts/VolumeSlider.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VolumeSlider : MonoBehaviour {
+
+	void Start(){
+		AudioListener.volume = 0.05f;
+	}
+
+	public void OnValueChanged(float val) {
+		Debug.Log ("hi");
+		AudioListener.volume = val;
+	}
+}
