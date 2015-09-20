e1e9a8ea4cb2b3f1eeb9654be6b7d0f2a52d1363
3burmest <frederik@burmester.eu>
2015-09-20 12:17:42 +0200

    Ok waren untracked

---
+++ SpaceRace/Assets/NoDoubleHUD.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoDoubleHUD : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		if(FindObjectsOfType(GetType()).Length > 1) {
+			Destroy(gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ SpaceRace/Assets/StartGame.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+	public string startLevel = "AsteroidenOrbit";
+
+	public void LoadNewLevel() {
+		Application.LoadLevel(startLevel);
+	}
+}
